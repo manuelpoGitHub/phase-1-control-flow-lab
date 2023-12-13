8d1ba753f83e050cffcc5a4753f1cb752d9bb787
Manny <manuelpo.msn.com>
2023-12-13 16:46:25 -0700

    finished lab

--- test/indexTest.js
+++ test/indexTest.js
@@ -2,22 +2,22 @@ require ( './helpers.js' );
 
 describe('index.js', function () {
   describe('scuberGreetingForFeet()', function () {
-    it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
-      expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
-    });
-
-    it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
-      expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
-    });
-
-    it('charges 30 dollars for a distance over 2000 feet', function () {
-      expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
-    });
-
-    it('does not allow rides over 2500 feet', function () {
-      expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
-    });
+  it('gives customers a free sample if the ride is less than or equal to 400 feet', function () {
+    expect(scuberGreetingForFeet(199)).to.equal('This one is on me!');
   });
+
+  it('charges 20 dollars for a distance between 400 and 2000 feet', function () {
+    expect(scuberGreetingForFeet(1500)).to.equal('That will be twenty bucks.');
+  });
+
+  it('charges 30 dollars for a distance over 2000 feet', function () {
+    expect(scuberGreetingForFeet(2001)).to.equal('I will gladly take your thirty bucks.');
+  });
+
+  it('does not allow rides over 2500 feet', function () {
+    expect(scuberGreetingForFeet(2501)).to.equal('No can do.');
+  });
+});
 
   describe('ternaryCheckCity()', function () {
     it('returns "Ok, sounds good." when the city is NYC', function () {
